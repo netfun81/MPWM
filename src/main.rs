7d0cf82816f5cb9b1e82fb09ef84c80ccb778311
netfun81 <netfun81@yahoo.com>
2022-05-10 12:48:33 -0600

    more minimal configuration

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,20 @@
 extern crate penrose;
 
 use penrose::{
-    contrib::{
-        extensions::Scratchpad,
-        hooks::{
-            LayoutSymbolAsRootName,
-        },
-        layouts::paper,
-    },
     core::{
-        config::Config,
-        helpers::index_selectors,
-        layout::{
-            bottom_stack,
-            side_stack,
-            Layout,
-            LayoutConf,
-        },
-        manager::WindowManager,
-        ring::Selector,
-    },
-    xcb::{
-        XcbConnection,
-        XcbHooks,
+        bindings::MouseEvent, config::Config, helpers::index_selectors, manager::WindowManager,
     },
     logging_error_handler,
-    Backward,
-    Forward,
-    Less,
-    More,
-    Result,
+    xcb::new_xcb_backed_window_manager,
+    Backward, Forward, Less, More, Result, Selector
 };
 
-use simplelog::{LevelFilter, SimpleLogger};
-use std::collections::HashMap;
-
 fn main() -> Result<()> {
-    SimpleLogger::init(LevelFilter::Debug, simplelog::Config::default())
-        .expect("failed to init logging");
-
+    let hooks = vec![];
+    
     let mut config_builder = Config::default().builder();
     config_builder
+		.workspaces(vec!["1", "2", "3", "4", "5"])
         .floating_classes(vec!["dmenu", "dunst", "polybar", "rofi"])
         .focused_border("#0000ff")?
         .unfocused_border("#0c1014")?
@@ -50,77 +24,45 @@ fn main() -> Result<()> {
         .show_bar(false)
         .top_bar(false)
         .bar_height(0);
-
-    let follow_focus_conf = LayoutConf {
-        floating: false,
-        gapless: true,
-        follow_focus: true,
-        allow_wrapping: false,
-    };
-
-    let n_main = 1;
-    let ratio = 0.5;
-
-    config_builder.layouts(vec![
-        Layout::new("[side]", LayoutConf::default(), side_stack, n_main, ratio),
-        Layout::new("[botm]", LayoutConf::default(), bottom_stack, n_main, ratio),
-        Layout::new("[papr]", follow_focus_conf, paper, n_main, ratio),
-        Layout::floating("[----]"),
-    ]);
-
     let config = config_builder.build().unwrap();
-    
-    let program_launcher = "rofi -show run";
-    let file_manager     = "thunar";
-    let terminal         = "alacritty";
-    let browser          = "chromium --incognito";
-    let mixer            = "pavucontrol";
-    let locker           = "slock";
-
-    let sp = Scratchpad::new(terminal, 0.8, 0.8);
-    let hooks: XcbHooks = vec![
-       LayoutSymbolAsRootName::new(),
-        sp.get_hook(),
-    ];
 
     let key_bindings = gen_keybindings! {
-        "M-r"   => run_external!(program_launcher);
-        "M-t"   => run_external!(terminal);
-        "M-e"   => run_external!(file_manager);
-        "M-w"   => run_external!(browser);
-        "M-a"   => run_external!(mixer);
-        "M-s"   => run_external!(locker);
-
-        "M-Tab"    => run_internal!(cycle_client, Forward);
-        "M-S-Tab"  => run_internal!(cycle_client, Backward);
-        "M-j"      => run_internal!(drag_client, Forward);
-        "M-k"      => run_internal!(drag_client, Backward);
-        "M-d"      => run_internal!(kill_client);
-        "M-f"      => run_internal!(toggle_client_fullscreen, &Selector::Focused);
-        "M-Return" => sp.toggle(); // popup terminal
-        "M-n"      => run_internal!(toggle_workspace);
-        
-        "M-grave"    => run_internal!(cycle_layout, Forward);
-        "M-S-grave"  => run_internal!(cycle_layout, Backward);
-        "M-Up"       => run_internal!(update_max_main, More);
-        "M-Down"     => run_internal!(update_max_main, Less);
-        "M-Right"    => run_internal!(update_main_ratio, More);
-        "M-Left"     => run_internal!(update_main_ratio, Less);
-        "M-S-s"      => run_internal!(detect_screens);
+        "M-Tab" => run_internal!(cycle_client, Forward);
+        "M-S-Tab" => run_internal!(cycle_client, Backward);
+        "M-j" => run_internal!(drag_client, Forward);
+        "M-k" => run_internal!(drag_client, Backward);
+        "M-d" => run_internal!(kill_client);
+        "M-f" => run_internal!(toggle_client_fullscreen, &Selector::Focused);
+        "M-n" => run_internal!(toggle_workspace);
+        "M-x" => run_internal!(cycle_workspace, Forward);
+        "M-z" => run_internal!(cycle_workspace, Backward);
+        "M-grave" => run_internal!(cycle_layout, Forward);
+        "M-S-grave" => run_internal!(cycle_layout, Backward);
+        "M-Up" => run_internal!(update_max_main, More);
+        "M-Down" => run_internal!(update_max_main, Less);
+        "M-Right" => run_internal!(update_main_ratio, More);
+        "M-Left" => run_internal!(update_main_ratio, Less);
         "M-S-Escape" => run_internal!(exit);
+        "M-w" => run_external!("chromium --incognito");
+        "M-e" => run_external!("thunar");
+        "M-r" => run_external!("rofi -show run");
+        "M-t" => run_external!("alacritty");
+        "M-a" => run_external!("pavucontrol");
+        "M-s" => run_external!("slock");
 
-        map: { "z", "x", "c", "v", "b" } to index_selectors(5) => {
+        map: { "1", "2", "3", "4", "5" } to index_selectors(5) => {
             "M-{}" => focus_workspace (REF);
             "M-S-{}" => client_to_workspace (REF);
         };
     };
 
-    let conn = XcbConnection::new()?;
+    let mouse_bindings = gen_mousebindings! {
+        Press Right + [Meta] => |wm: &mut WindowManager<_>, _: &MouseEvent| wm.cycle_workspace(Forward),
+        Press Left + [Meta] => |wm: &mut WindowManager<_>, _: &MouseEvent| wm.cycle_workspace(Backward)
+    };
 
-    let mut wm = WindowManager::new(config, conn, hooks, logging_error_handler());
-    wm.init()?;
-    
-    wm.grab_keys_and_run(key_bindings, HashMap::new())?;
+    let mut wm = new_xcb_backed_window_manager(config, hooks, logging_error_handler())?;
+    wm.grab_keys_and_run(key_bindings, mouse_bindings)?;
 
     Ok(())
 }
