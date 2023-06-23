6fad7b8faeaef7d3bfe2f5e0ac4e0f6ca8f8042a
netfun81 <netfun81@yahoo.com>
2023-06-22 18:04:45 -0600

    upgrade to penrose 3.2 and revert to minimal config

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
 use penrose::{
     builtin::{
-        actions::{exit, log_current_state, modify_with, send_layout_message, spawn},
-        layout::{
-            messages::{ExpandMain, IncMain, ShrinkMain},
-            transformers::{Gaps, ReflectHorizontal, ReserveTop},
-            MainAndStack,
-        },
+        actions::{exit, modify_with, send_layout_message, spawn},
+        layout::messages::{ExpandMain, IncMain, ShrinkMain},
     },
     core::{
         bindings::{parse_keybindings_with_xmodmap, KeyEventHandler},
-        layout::LayoutStack,
         Config, WindowManager,
     },
-    extensions::hooks::{add_ewmh_hooks}, //SpawnOnStartup},
-    map, stack,
+    map,
     x11rb::RustConn,
     Result,
 };
@@ -22,31 +16,28 @@ use tracing_subscriber::{self, prelude::*};
 
 fn raw_key_bindings() -> HashMap<String, Box<dyn KeyEventHandler<RustConn>>> {
     let mut raw_bindings = map! {
-        // map_keys: |k: &str| format!("C-{k}");
-        map_keys: |k: &str| k.to_owned();
+        map_keys: |k: &str| k.to_string();
 
         "M-j" => modify_with(|cs| cs.focus_down()),
         "M-k" => modify_with(|cs| cs.focus_up()),
         "M-S-j" => modify_with(|cs| cs.swap_down()),
         "M-S-k" => modify_with(|cs| cs.swap_up()),
         "M-S-d" => modify_with(|cs| cs.kill_focused()),
-        "M-Tab" => modify_with(|cs| cs.toggle_tag()),
-        "M-Up" => send_layout_message(|| IncMain(1)),
-        "M-Down" => send_layout_message(|| IncMain(-1)),
-        "M-Right" => send_layout_message(|| ExpandMain),
-        "M-Left" => send_layout_message(|| ShrinkMain),
-        "M-S-s" => log_current_state(),
-        "M-w" => spawn("brave-browser"),
-        "M-e" => spawn("pcmanfm-qt"),
+        "M-S-Up" => send_layout_message(|| IncMain(1)),
+        "M-S-Down" => send_layout_message(|| IncMain(-1)),
+        "M-S-Right" => send_layout_message(|| ExpandMain),
+        "M-S-Left" => send_layout_message(|| ShrinkMain),
+        "M-w" => spawn("firefox"),
+        "M-e" => spawn("thunar"),
         "M-r" => spawn("dmenu_run"),
-        "M-t" => spawn("qterminal"),
-        "M-a" => spawn("pavucontrol-qt"),
+        "M-t" => spawn("alacritty"),
+        "M-a" => spawn("pavucontrol"),
         "M-s" => spawn("slock"),
-        "M-p" => spawn("systemctl suspend"),
+        "M-S-s" => spawn("systemctl suspend"),
         "M-S-q" => exit(),
     };
 
-    for tag in &["1", "2", "3", "4", "5", "6", "7", "8", "9"] {
+    for tag in &["1", "2", "3", "4", "5", "6"] {
         raw_bindings.extend([
             (
                 format!("M-{tag}"),
@@ -62,37 +53,29 @@ fn raw_key_bindings() -> HashMap<String, Box<dyn KeyEventHandler<RustConn>>> {
     raw_bindings
 }
 
-fn layouts() -> LayoutStack {
-    let max_main = 1;
-    let ratio = 0.6;
-    let ratio_step = 0.1;
-    let outer_px = 0;
-    let inner_px = 0;
-    let top_px = 0;
-
-    stack!(
-        MainAndStack::side(max_main, ratio, ratio_step),
-        ReflectHorizontal::wrap(MainAndStack::side(max_main, ratio, ratio_step)),
-        MainAndStack::bottom(max_main, ratio, ratio_step)
-    )
-    .map(|layout| ReserveTop::wrap(Gaps::wrap(layout, outer_px, inner_px), top_px))
-}
-
 fn main() -> Result<()> {
     tracing_subscriber::fmt()
-        .with_env_filter("trace")
+        .with_env_filter("info")
         .finish()
         .init();
 
-    let config = add_ewmh_hooks(Config {
-        default_layouts: layouts(),
-        //startup_hook: Some(SpawnOnStartup::boxed("polybar")),
-        ..Config::default()
-    });
-
     let conn = RustConn::new()?;
     let key_bindings = parse_keybindings_with_xmodmap(raw_key_bindings())?;
-    let wm = WindowManager::new(config, key_bindings, HashMap::new(), conn)?;
+    let wm = WindowManager::new(Config::default(), key_bindings, HashMap::new(), conn)?;
 
     wm.run()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bindings_parse_correctly_with_xmodmap() {
+        let res = parse_keybindings_with_xmodmap(raw_key_bindings());
+
+        if let Err(e) = res {
+            panic!("{e}");
+        }
+    }
 }
